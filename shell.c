02ac4c50937c77417c2ff0e18f83b6e8577ba8e3
Valentinaga1 <1881@holbertonschool.com>
2020-08-17 17:01:13 +0000

    Main function of our shell

---
+++ shell.c
@@ -0,0 +1,39 @@
+#include "shell.h"
+/**
+ * main - 
+ * 
+ */
+int main() 
+{
+	char  **tokens = NULL, *buffer = NULL;
+	size_t size;
+	int c;
+
+	while(1)
+	{
+		if((write(1, "$ ", 2)) == -1)
+		{
+			perror("Error");
+			return (1);
+		}
+		c = getline(&buffer, &size, stdin);
+		if((_strcmp(buffer,"exit\n") == 0))
+		{
+			free(buffer);
+			exit(EXIT_SUCCESS);
+		}
+		if (c == -1)
+		{
+			perror("Error");
+			free(buffer);
+			return(1);
+		}
+			if(buffer == NULL)
+				return (1);
+			tokens = tokenize(buffer);
+			execute(tokens);
+			free(tokens);
+	}
+	free(buffer);
+	return 0;
+}
