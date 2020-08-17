6abf8413dcc90b82b1483a8898742602d444544f
DanielVillalba-holberton <1821@holbertonschool.com>
2020-08-17 17:17:23 +0000

    function to Extract tokens from strings

---
+++ tokenize.c
@@ -0,0 +1,25 @@
+#include "shell.h"
+/**
+ * tokenize - Extract tokens from strings
+ * @str: String to be parser.
+ * @delim: Delimiter character.
+ * Return: Tokenized string.
+ */
+char **tokenize(char *str)
+{
+	char *token = NULL, **tok = NULL;
+	int i;
+	tok = malloc(sizeof(char *) * 1024);
+	if (tok == NULL)
+		exit(0);
+	token = strtok(str, " \n");
+	i = 0;
+	while (token != NULL)
+	{
+		tok[i] = token;
+		token = strtok(NULL, " \n");
+		i++;
+	}
+	tok[i] = NULL; 
+	return (tok);
+}
