5f3d76d7dfd33ce37bd38712627abb053cb41a5b
Valentinaga1 <1881@holbertonschool.com>
2020-08-17 16:59:49 +0000

    Function to print strings

---
+++ _putchar.c
@@ -0,0 +1,16 @@
+#include "shell.h"
+
+/**
+ * _putchar - writes the character c to stdout
+ * @str: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void _putchar(char *str)
+{
+	int len;
+
+	len = strlen(str);
+	write(1, str, len);
+}
