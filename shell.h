532a234c18f7b8cbffd536c7aaeaa9ca6490b096
DanielVillalba-holberton <1821@holbertonschool.com>
2020-08-17 17:15:19 +0000

    header file

---
+++ shell.h
@@ -0,0 +1,20 @@
+#ifndef SHELL_H_
+#define SHELL_H_
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <sys/types.h>
+#include <signal.h>
+	extern char **environ;
+char **tokenize(char *str);
+void execute(char **tokens);
+void printenvironment(void);
+void _putchar(char *str);
+char * _getenv(char *name);
+char *_strdup(char *str);
+char * str_cat(char *str1, char slash, char *str2);
+int _strcmp(char *s1, char *s2);
+#endif
