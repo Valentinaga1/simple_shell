2abb430c4281ea3abcbf44fa1aa89c3b2f31799c
Valentinaga1 <1881@holbertonschool.com>
2020-08-18 21:33:44 +0000

    Fixed Betty

--- _getenv.c
+++ _getenv.c
@@ -1,33 +1,33 @@
 #include "shell.h"
 /**
- * _getenv - 
- * 
- * 
+ *_getenv - Function to get the environment variable.
+ * @path: Environment variable to looking for.
+ * Return: Null.
  */
 char *_getenv(char *path)
 {
-    char *token = NULL, **tok = NULL;
+	char *token = NULL, **tok = NULL;
 	int i = 0, j = 0;
-    tok = malloc(sizeof(char *) * 1024);
-    while (environ[j])
-    {
-        token = strtok(environ[j], "=");
-        while (token != NULL)
-        {
-            tok[i] = token;
-            token = strtok(NULL, "=");
-            i++;
-        }
-        j++;
-    }
-    tok[i] = NULL;
-    i = 0;
-	while(tok[i])
-    {
-        if(_strcmp(tok[i],path) == 0)
-            return(tok[i+1]);
-        i++;
-    }
 
-    return(NULL);
+	tok = malloc(sizeof(char *) * 1024);
+	while (environ[j])
+	{
+		token = strtok(environ[j], "=");
+		while (token != NULL)
+		{
+			tok[i] = token;
+			token = strtok(NULL, "=");
+			i++;
+		}
+		j++;
+	}
+	tok[i] = NULL;
+	i = 0;
+	while (tok[i])
+	{
+		if (_strcmp(tok[i], path) == 0)
+			return (tok[i + 1]);
+		i++;
+	}
+	return (NULL);
 }
